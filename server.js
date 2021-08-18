ac81a6ac60eedc2c117edddabb4f0cb966b4b2ab
Aubri Henley <aubri.henley@gmail.com>
2021-08-18 16:48:48 -0400

    added routes for html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
-
+// const api = require('./routes/index.js');
+const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -9,19 +9,11 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
+app.use('/', htmlRoutes);
+// app.use('/api', api);
 
 app.use(express.static('public'));
-
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
